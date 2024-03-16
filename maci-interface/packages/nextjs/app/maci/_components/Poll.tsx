9906d57917dae4792c80e48f80c621fb3be0b170
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-03-16 01:45:21 +0000

    feat: Added time machine for fast forwarding

--- maci-interface/packages/nextjs/app/maci/_components/Poll.tsx
+++ maci-interface/packages/nextjs/app/maci/_components/Poll.tsx
@@ -8,9 +8,8 @@ import { useAccount, useContractRead, useContractWrite, useNetwork } from "wagmi
 import { displayTxResult } from "~~/app/debug/_components/contract";
 import { useDeployedContractInfo, useTransactor } from "~~/hooks/scaffold-eth";
 import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
-import { getParsedError } from "~~/utils/scaffold-eth";
+import { getParsedError, notification } from "~~/utils/scaffold-eth";
 import { getAllContracts } from "~~/utils/scaffold-eth/contractsData";
-
 
 export const r = BigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617");
 
@@ -38,12 +37,37 @@ export function Poll({ address }: { address: string }) {
   const contractsData = getAllContracts();
 
   const { data: deployedContractData, isLoading: deployedContractLoading } = useDeployedContractInfo("Poll");
-  const readMethods = ["numMessages", "numSignups", "coordinatorPubKey"];
+  const readMethods = ["numMessages", "numSignups", "coordinatorPubKey", "finished"];
   const [numMsg, setNumMsg] = useState<any>();
   const [numSignups, setNumSignups] = useState<any>();
+  const [finished, setFinished] = useState<any>();
   const [coordinatorPubKey, setCoordinatorPubKey] = useState<any>();
   const queryResults = new Map<string, any>();
-  const setStateVariables = [setNumMsg, setNumSignups, setCoordinatorPubKey];
+  const setStateVariables = [setNumMsg, setNumSignups, setCoordinatorPubKey, setFinished];
+
+  const {
+    data: fini,
+    isLoading: isFinishedLoading,
+    writeAsync: finishAsync,
+  } = useContractWrite({
+    address: address,
+    functionName: "finish",
+    abi: contractsData["Poll"].abi,
+    args: [true],
+  });
+
+  const handleFinish = async () => {
+    if (finishAsync) {
+      try {
+        const makeWriteWithParams = () => finishAsync();
+        await writeTxn(makeWriteWithParams);
+        // onChange();
+      } catch (e: any) {
+        const message = getParsedError(e);
+        notification.error(message);
+      }
+    }
+  };
 
   readMethods.forEach(methodName => {
     queryResults.set(
@@ -75,7 +99,6 @@ export function Poll({ address }: { address: string }) {
     }
 
     readMethods.map((value, key) => {
-      console.log("ciao value", value);
       fetchData(queryResults.get(value).isFetching, queryResults.get(value).refetch, setStateVariables[key]);
     });
   }, [deployedContractData]);
@@ -90,7 +113,21 @@ export function Poll({ address }: { address: string }) {
         // textAlign: "center",
       }}
     >
-      <h1>Poll Id 1</h1>
+      <h1>Poll Id 0</h1>
+      <br></br>
+
+      <h1>State</h1>
+      <p>{displayTxResult(finished)}</p>
+      <button
+        onClick={() => {
+          handleFinish();
+        }}
+        className="btn btn-secondary btn-sm"
+      >
+        TIME-MACHINE
+      </button>
+      <br></br>
+
       <h1>Num Messages</h1>
       <p>{numMsg && displayTxResult(numMsg)}</p>
       <h1>Num Signups</h1>
