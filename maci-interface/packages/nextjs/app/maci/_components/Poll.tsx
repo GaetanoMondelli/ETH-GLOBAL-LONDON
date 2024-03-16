53b8b83152ea540c90189f86369ca87c0a7995c8
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-03-16 00:46:39 +0000

    feat: adding Poll View

---
+++ maci-interface/packages/nextjs/app/maci/_components/Poll.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import React, { use, useEffect, useState } from "react";
+import { MaciPubKeyViewer } from "./MaciPubKeyViewer";
+import { BigNumber } from "@ethersproject/bignumber";
+import { Keypair, PrivKey, PubKey } from "maci-domainobjs";
+import { useAccount, useContractRead, useContractWrite, useNetwork } from "wagmi";
+import { displayTxResult } from "~~/app/debug/_components/contract";
+import { useDeployedContractInfo, useTransactor } from "~~/hooks/scaffold-eth";
+import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
+import { getParsedError } from "~~/utils/scaffold-eth";
+import { getAllContracts } from "~~/utils/scaffold-eth/contractsData";
+
+
+export const r = BigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+
+export const genKeyPair = ({ seed, quiet = true }: any): { publicKey: string; privateKey: string } => {
+  // create the new random keypair if there is no seed value
+  const keypair = new Keypair(seed ? new PrivKey(seed % r) : undefined);
+
+  // serialize both private and public keys
+  const serializedPubKey = keypair.pubKey.serialize();
+  const serializedPrivKey = keypair.privKey.serialize();
+
+  return {
+    publicKey: serializedPubKey,
+    privateKey: serializedPrivKey,
+  };
+};
+
+export function Poll({ address }: { address: string }) {
+  const writeTxn = useTransactor();
+  const { chain } = useNetwork();
+  const { address: connectedAddress } = useAccount();
+
+  const { targetNetwork } = useTargetNetwork();
+
+  const contractsData = getAllContracts();
+
+  const { data: deployedContractData, isLoading: deployedContractLoading } = useDeployedContractInfo("Poll");
+  const readMethods = ["numMessages", "numSignups", "coordinatorPubKey"];
+  const [numMsg, setNumMsg] = useState<any>();
+  const [numSignups, setNumSignups] = useState<any>();
+  const [coordinatorPubKey, setCoordinatorPubKey] = useState<any>();
+  const queryResults = new Map<string, any>();
+  const setStateVariables = [setNumMsg, setNumSignups, setCoordinatorPubKey];
+
+  readMethods.forEach(methodName => {
+    queryResults.set(
+      methodName,
+      useContractRead({
+        address: address,
+        functionName: methodName,
+        abi: deployedContractData?.abi as any,
+        args: [],
+        enabled: false,
+        onError: (error: any) => {
+          const parsedErrror = getParsedError(error);
+          console.log(parsedErrror);
+        },
+      }),
+    );
+  });
+
+  useEffect(() => {
+    async function fetchData(isFetching: any, refetch: any, setData: any) {
+      if (isFetching) {
+        return;
+      }
+      if (refetch) {
+        const { data } = await refetch();
+        console.log("ciao key", data);
+        setData(data);
+      }
+    }
+
+    readMethods.map((value, key) => {
+      console.log("ciao value", value);
+      fetchData(queryResults.get(value).isFetching, queryResults.get(value).refetch, setStateVariables[key]);
+    });
+  }, [deployedContractData]);
+
+  return (
+    <div
+      className="card"
+      style={{
+        padding: 20,
+        margin: 20,
+        width: 800,
+        // textAlign: "center",
+      }}
+    >
+      <h1>Poll Id 1</h1>
+      <h1>Num Messages</h1>
+      <p>{numMsg && displayTxResult(numMsg)}</p>
+      <h1>Num Signups</h1>
+      <p>{numSignups && displayTxResult(numSignups)}</p>
+      <h1>Coordinator Public Key</h1>
+      {coordinatorPubKey && <MaciPubKeyViewer address={new PubKey(coordinatorPubKey).serialize()} />}
+      {coordinatorPubKey && (
+        <>
+          <p>
+            Part1:
+            {BigInt(coordinatorPubKey[0]).toString()}
+          </p>
+          <p>
+            Part2:
+            {BigInt(coordinatorPubKey[1]).toString()}
+          </p>
+        </>
+      )}
+
+      <br></br>
+      <br></br>
+    </div>
+  );
+}
