69186f0621608a4f1f0478c46d4670d011529687
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-03-17 04:29:59 +0000

    feat: maci account with qr code example

--- maci-interface/packages/nextjs/app/maci/_components/MaciPubKeyViewer.tsx
+++ maci-interface/packages/nextjs/app/maci/_components/MaciPubKeyViewer.tsx
@@ -9,9 +9,12 @@ export const r = BigInt("2188824287183927522224640574525727508854836440041603434
 export function MaciPubKeyViewer({ address }: { address: string }) {
   return (
     <div>
+      {/* https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=mkci */}
       <Avatar 
         // style={{ border: "1px solid #000000", backgroundColor: "white"}}
-      size={64} src={`https://robohash.org/${address}`}></Avatar>
+      // size={64} src={`https://robohash.org/${address}`}></Avatar>
+      size={64} src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${address}`}></Avatar>
+
       <p>
         <Tooltip title={address}>
           <Tag color="blue">{address.slice(7, 11).concat("...").concat(address.slice(-4))}</Tag>
